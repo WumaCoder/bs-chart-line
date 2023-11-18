cbfce0c3eb827b9f151141c32a1a4358c4ae0f75
WumaCoder <1829913225@qq.com>
2023-11-18 18:54:36 +0800

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,11 @@
+const { i18n } = require("./next-i18next.config");
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  i18n,
+  productionBrowserSourceMaps: true,
+  reactStrictMode: true,
+  transpilePackages: ['@douyinfe/semi-ui', '@douyinfe/semi-icons', '@douyinfe/semi-illustrations'],
+}
+
+module.exports = nextConfig
